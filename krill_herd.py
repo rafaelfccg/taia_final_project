c4246d6e87785da4b09c6a8f19bedb9e9efef251
vinimoraesrc <vmrc@cin.ufpe.br>
2016-11-20 16:20:46 -0300

    fixes typo

--- krill_herd.py
+++ krill_herd.py
@@ -30,7 +30,7 @@ Xbest = (list(), 1e10, list(), 1e10)
 # Begin helpers
 ############################################
 
-# Implemented acoording to PEP 485
+# Implemented according to PEP 485
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
