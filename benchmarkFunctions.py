1e22d1d931eb6b43a79ae1b17a095deb9ed26e96
jpedrocm <jpcm@cin.ufpe.br>
2016-11-21 19:48:46 -0300

    Defining easy calls for func params

--- benchmarkFunctions.py
+++ benchmarkFunctions.py
@@ -43,6 +43,9 @@ def ackley(genome):
 	b = (sum2/n)
 	return -20 * math.exp(a) - math.exp(b) + 20 + math.e
 
+def ACKLEY():
+	return [ackley, 20, 10**-3, ackley_min_x, ackley_max_x]
+
 # minimum 0
 def griewank (genome):
 	n = len(genome)
@@ -52,11 +55,17 @@ def griewank (genome):
 
 	return sumi - prod
 
+def GRIEWANK():
+	return [griewank, 20, 10**-3, griewank_min_x, griewank_max_x]
+
 # minimum 0
 def rastrigin (genome):
 	n = len(genome)
 	sumi = 10 * n + sum(map(lambda x : x**2 - 10 * math.cos(2 * math.pi * x), genome))
 	return sumi
+
+def RASTRIGIN():
+	return [rastrigin, 20, 10**-3, rastrigin_min_x, rastrigin_max_x]
 
 # minimum 0
 def rosenbrock(genome):
@@ -64,16 +73,28 @@ def rosenbrock(genome):
 	sumi = sum([100 * (genome[i+1] - genome[i]**2)**2 + (genome[i] - 1)**2 for i in range(n-1)])
 	return sumi
 
+def ROSENBROCK():
+	return [rosenbrock, 20, 10**-3, rosenbrock_min_x, rosenbrock_max_x]
+
 # minimum 0
 def Schwefel226(genome):
 	return (sum(map(lambda x : -x * math.sin(math.sqrt(math.fabs(x))), genome)))
+
+def SCHWEFEL226():
+	return [Schwefel226, 20, 10**-3, schwefel226_min_x, schwefel226_max_x]
 
 def Schwefel222(genome):
 	modified_genome = map(lambda x : math.fabs(x), genome)
 	return sum(modified_genome) + product(modified_genome)
 
+def SCHWEFEL222():
+	return [Schwefel222, 20, 10**-3, schwefel222_min_x, schwefel222_max_x]
+
 def Sphere(genome):
 	return math.sqrt(sum(map(lambda x : x**2, genome)))
+
+def SPHERE():
+	return [Sphere, 20, 10**-3, sphere_min_x, sphere_max_x]
 
 # 2 dimensions
 
@@ -94,6 +115,9 @@ def branin(genome):
 
 	return a * ((genome[1] - b * (genome[0]**2) + c * genome[0] - d)**2) * g *(1 - h) * math.cos(genome[0]) + g
 
+def BRANIN():
+	return [branin, 2, 5/(4*math.pi), branin_min_x, branin_max_x, branin_min_y, branin_max_y]
+
 # # dimensions 4
 # PS: Ta faltando valores na tabela de hartman1 que ele menciona no artigo
 # hartman1_max_x = 1
@@ -107,5 +131,3 @@ def branin(genome):
 # 	for x in xrange(1,10):
 # 		pass
 # 	return
-
-print (Schwefel222([1,1]))
